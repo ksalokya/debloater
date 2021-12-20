5ed4b89087925fff79f731b4e8e98269caa40161
Salokya Kumar <kumar.salokya007@gmail.com>
2021-12-20 10:18:23 +0530

    Update debloater_for_linux.sh

--- debloater_for_linux.sh
+++ debloater_for_linux.sh
@@ -21,21 +21,6 @@ ascii_intro()
     printf "\n                                          /                /    "
 }
 
-spinner()
-{
-    local pid=$!
-    local delay=0.25
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr#"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
-}
-
 brief()
 {
     printf "\n\nMake sure you have read the FAQ in te following link before running this script:"
